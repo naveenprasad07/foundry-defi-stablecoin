7af773600c673e5ff455b5b942b05849164e9cda
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-03 17:01:17 +0000

    update

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,13 +4,26 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployDSC is Script{
-    function run() external returns (DecentralizedStableCoin,DSCEngine){
-        vm.startBroadcast();
+contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        // DSCEngine dsc = new DSCEngine();
-        
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engine));
         vm.stopBroadcast();
+
+        return (dsc, engine, config);
     }
 }
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -120,8 +120,17 @@ contract DSCEngine is ReentrancyGuard {
     ////////////////////////
     // External Functions //
     ////////////////////////
-
-    function depositCollateralAndMintDsc() external {}
+/**
+ * 
+ * @param tokenCollateralAddress The address of the token to deposit as collateral
+ * @param amountCollateral The amount fo collateral to deposit
+ * @param amountDscToMint The amount of decentralized stablecoin to mint
+ * @notice this function will deposit your collateral and mint DSC in one transaction
+ */
+    function depositCollateralAndMintDsc(address tokenCollateralAddress,uint256 amountCollateral,uint256 amountDscToMint) external {
+        depositCollateral(tokenCollateralAddress,amountCollateral);
+        mintDsc(amountDscToMint);
+    }
 
     /**
      * @notice follows CEI
@@ -129,7 +138,7 @@ contract DSCEngine is ReentrancyGuard {
      * @param amountCollateral The amount of collateral to deposit
      */
     function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
-        external
+        public
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
@@ -151,7 +160,7 @@ contract DSCEngine is ReentrancyGuard {
      * @param amountDscToMint The amount of decentralized stablecoin to mint
      * @notice they must have more collateral value than the minimum threshold
      */
-    function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
+    function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
         // if they minted too much ($150 DSC, $100 ETH)
         _reverseIfHealthFactorIsBroken(msg.sender);
