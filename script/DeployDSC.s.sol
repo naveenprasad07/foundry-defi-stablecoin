4d98d7a4f662fa69b20747106f038d8933acd8ec
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-05 05:06:29 +0000

    final-commit

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -1,29 +1,28 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
-import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
-import {DSCEngine} from "../src/DSCEngine.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+import { Script } from "forge-std/Script.sol";
+import { HelperConfig } from "./HelperConfig.s.sol";
+import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
+import { DSCEngine } from "../src/DSCEngine.sol";
 
 contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
     function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
-        HelperConfig config = new HelperConfig();
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
 
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
-            config.activeNetworkConfig();
+            helperConfig.activeNetworkConfig();
         tokenAddresses = [weth, wbtc];
         priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
         vm.startBroadcast(deployerKey);
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
-        dsc.transferOwnership(address(engine));
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(dscEngine));
         vm.stopBroadcast();
-
-        return (dsc, engine, config);
+        return (dsc, dscEngine, helperConfig);
     }
 }
